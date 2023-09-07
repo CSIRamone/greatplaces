1cd0a38221b5c90aa4e6d7cbf19934f12ba1242d
CSIRamone <cidinei_inacio@hotmail.com>
2023-09-06 23:00:34 -0300

    Evoluindo Componente ImageInput

--- lib/widget/image_input.dart
+++ lib/widget/image_input.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
-  const ImageInput({super.key});
+  final Function onSelectImage;
+
+  const ImageInput(this.onSelectImage, {Key? key}) : super(key: key);
 
   @override
   State<ImageInput> createState() => _ImageInputState();
@@ -25,6 +27,8 @@ class _ImageInputState extends State<ImageInput> {
     setState(() {
       _storedImage = File(imageFile.path);
     });
+
+    //widget.onSelectImage(...);
   }
 
   @override
